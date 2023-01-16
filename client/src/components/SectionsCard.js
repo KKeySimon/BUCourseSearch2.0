c6e5fe13a26570f1ad05f8072a4a367141fe8ffc
matthew-iwane <mattiwanee@gmail.com>
2023-01-15 23:19:34 -0800

    added locations to the output of courses

--- client/src/components/SectionsCard.js
+++ client/src/components/SectionsCard.js
@@ -2,6 +2,7 @@ const SectionsCard =  ({ sections }) => {
     return (
         <div>
             <p>{sections.section_full_name + " --- " +  sections.instructor}</p>
+            <p>{sections.location}</p>
         </div>
     )
 }
