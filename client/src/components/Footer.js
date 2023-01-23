1e303d0b8f0c1b9efcf8bf126864b2f9f0835eb6
Matthew T. Iwane <mattiwane@gmail.com>
2023-01-23 16:06:28 -0500

    changed footer and added github logo, deleted redundant info

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,28 +1,20 @@
 import * as React from "react";
-import bulogo from "../assets/bu.png"
+import github from "../assets/github.png"
 
 export default function Footer() {
   return (
     <footer className="coursearch-footer">
       <div className="container">
         <ul className="coursearch-footer-links">
-          <li><a href="https://www.bu.edu/academics/bulletin/">Bulletin</a></li>
-
-          <li><a href="https://www.bu.edu/link/bin/uiscgi_studentlink.pl/uismpl/?ModuleName=menu.pl&NewMenu=Home">Student Link</a></li>
-
-          <li><a href="http://www.bu.edu/help/tech/">Contact</a></li>
-
-          <li><a href="https://www.bu.edu/advising/">Undergraduate Advising</a></li>
+          <li>
+            <a className="coursearch-footer-masterplate" href="https://github.com/KKeySimon/BUCourseSearch2.0">
+              <img src={github} alt="GitHub" className="github-img" />
+            </a>
+          </li>
         </ul>
 
         <div className="coursearch-footer-copyright">
           <p>© 2022 Boston University Course Search 2.0</p>
-        </div>
-
-        <div className="coursearch-footer-branding">
-          <a className="coursearch-footer-masterplate" href="https://www.bu.edu">
-          <img src={bulogo} alt="bu-logo"></img>
-        </a>
         </div>
       </div>
     </footer>
